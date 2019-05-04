4f7eae25c97631f1d46770fc12887c94b71e6219
akshatsaxena1010 <akshatonlyone@gmail.com>
2019-05-04 14:16:52 +0530

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -27,8 +27,10 @@ class safari
 		int getHash(char key[]);
 		void display();
 		void unpack();
-		void search();
-		//void retrieve(int addr, char k[]);		
+		int search(char skey[]);
+		void dispRec(int addr);
+		void delRec(int addr);
+		void modify(int addr);
 };
 
 void safari::initial()
@@ -46,14 +48,9 @@ void safari::initial()
 	
 	for(i=0; i<max; i++)
 	{	
-	//	cout<<i<<endl;
-		
-		//cout<<check<<endl;
 		file.seekg(i*(recsize),ios::beg);
 		int f=file.tellg();
-	//	cout<<"\nf="<<f;
 		file.getline(check, recsize, '|');
-	//	cout<<check<<endl;
 		if(strcmp(check,"#####################################") == 0)
 			strcpy(keys[i], "NULL");
 		else 
@@ -61,16 +58,13 @@ void safari::initial()
 		file.getline(dummy, recsize, '\n');
 	}
 	file.close();
-	
-//	for(i=0; i<max; i++)
-//		cout<<keys[i]<<endl;
 }
 
 void safari::reset()
 {
 	int i,j;
 	for(i=0; i<max; i++)
-		strcpy(keys[i],"null");
+		strcpy(keys[i],"NULL");
 	
 	file.open(safarifile);
 	if(!file)
@@ -85,7 +79,7 @@ void safari::reset()
 		file.seekp(i*(recsize),ios::beg);
 		for(j=0;j<recsize-3;j++)
 			file<<"#";
-		file<<"";
+		file<<"|";
 		file<<"\n";
 	}
 	cout<<"Empty file created!";
@@ -100,6 +94,12 @@ void safari::read()
 	cin.ignore();
 	cout<<"Enter your booking id"<<endl;
 	gets(bookId);
+	while(search(bookId) >= 0)
+	{
+		cout<<"\nBooking ID already exists"<<endl;
+		cout<<"Enter another booking id: ";
+		gets(bookId);
+	}
 	cout<<"Enter your name"<<endl;
 	gets(name);
 	system("cls");
@@ -128,6 +128,19 @@ void safari::pack()
 {
 	int addr;
 	addr = getHash(bookId);
+	while(strcmp(keys[addr], "NULL") != 0)
+	{
+		addr++;
+		if(addr == max)
+		{
+			addr = 0;
+		}
+		if(addr == getHash(bookId))
+		{
+			cout<<"\nOverflow of Data!";
+			return;
+		}
+	}
 	strcpy(keys[addr],bookId);
 	strcpy(buffer, bookId);
 	strcat(buffer,"|");
@@ -154,14 +167,13 @@ int safari::getHash(char key[])
 		sum=sum+key[i]-48;
 		i++;
 	}
-	cout<<sum;
 	return sum % max;
 }
 
 void safari::display()
 {
 	int i;
-	char dummy[recsize];
+	char dummy[recsize],check[recsize];
 	cout<<setw(6)<<"bookId"<<setw(20)<<"name"<<endl;
 	file.open(safarifile, ios::in);
 	if(!file)
@@ -172,68 +184,87 @@ void safari::display()
 	}	
 	for(i=0; i<max; i++)
 	{
-		file.getline(dummy, recsize, '|');
-		if(strcmp(dummy,"#####################################") != 0)
+		file.seekg(i*(recsize),ios::beg);
+		int f=file.tellg();
+		file.getline(check, recsize, '|');
+		if(strcmp(check,"#####################################") != 0)
 		{
-			file.getline(bookId, 6, '|');
+			strcpy(bookId,check);
 			file.getline(name, 20, '|');
 			cout<<setw(6)<<bookId<<setw(20)<<name<<endl;
-			file.getline(dummy, recsize, '|');
 		}
-		else
-			file.getline(dummy, recsize, '|');
 	}
 	cout<<"Press any key to continue"<<endl;
 	getch();
 	file.close();
 }
 
-void safari::unpack()
+int safari::search(char skey[6])
 {
-	char dummy[recsize];
-	
-	file.getline(dummy, recsize, '\n');
-}
-
-void safari::search()
-{
-	char skey[6];
 	int addr, i=0, flag=0;
-	cout<<"\nEnter the Booking ID ";
-	cin>>skey;
 	addr = getHash(skey);
 	i = addr;
 	do
 	{
-		if(keys[addr] == skey)
+		if(strcmp(keys[i],skey) == 0)
 		{
-			cout<<"\nBooking Found!!\n";
-			unpack();
-			cout<<"Booking ID:"<<bookId<<endl;
-			cout<<"Name: "<<name<<endl;
 			flag = 1;
-			cout<<"Press any key to continue....";
-			getch();
+			return i;
 		}
 		i++;
-		if(i == recsize)
+		if(i == max)
 			i=0;
+		if(keys[i] == "NULL")
+			break;
 	}while(i != addr);
-	
-	if(flag == 0)
+	return -1;
+}
+
+void safari::dispRec(int addr)
+{
+
+	file.open(safarifile);
+	if(!file)
 	{
-		cout<<"\nBooking not Found!!\n";
-		cout<<"Press any key to continue....";
+		cout<<"\nUnable to Open the File";
 		getch();
+		exit(1);
 	}
+	file.seekg(addr*(recsize),ios::beg);
+	int f=file.tellg();
+	file.getline(bookId, 6, '|');
+	file.getline(name, 20, '|');
+	cout<<"Booking ID:"<<bookId<<endl;
+	cout<<"Name: "<<name<<endl;
+	file.close();
+	cout<<"Press any key to continue....";
+	getch();
+}
+
+void safari::delRec(int addr)
+{
+	int j;
+	file.open(safarifile);
+	if(!file)
+	{
+		cout<<"\nUnable to Open the File";
+		getch();
+		exit(1);
+	}
+	file.seekp(addr*(recsize),ios::beg);
+	for(j=0;j<recsize-3;j++)
+		file<<"#";
+	file<<"|";
+	file.close();
+	strcpy(keys[addr], "NULL");
 }
 
 int main()
 {
 	safari s;
 	s.initial();
-	int ch;
-	char ex;
+	int ch, addr;
+	char ex, skey[6];
 	for(;;)
 	{
 		system("cls");
@@ -249,7 +280,60 @@ int main()
 			case 2: s.display();
 					break;
 					
-			case 3: s.search();
+			case 3: cout<<"\nEnter the Booking ID ";
+					cin>>skey;
+					addr = s.search(skey);
+					if(addr == -1)
+					{
+						cout<<"\nBooking not found!!"<<endl;
+						cout<<"Press any key to continue"<<endl;
+						getch();
+					}
+					else
+					{
+						cout<<"\nBooking Found!!\n";
+						s.dispRec(addr);
+					}
+					break;
+					
+			case 4: cout<<"\nEnter the Booking ID to modify: ";
+					cin>>skey;
+					addr = s.search(skey);
+					if(addr == -1)
+					{
+						cout<<"\nBooking not found!!"<<endl;
+						cout<<"Press any key to continue"<<endl;
+						getch();
+					}
+					else
+					{
+						s.dispRec(addr);
+						s.delRec(addr);
+						s.read();
+						cout<<"\nBooking Modified!"<<endl;
+						cout<<"Press any key to continue"<<endl;
+						getch();
+					}
+					break;
+					
+			case 5: cout<<"\nEnter the Booking ID to delete: ";
+					cin>>skey;
+					addr = s.search(skey);
+					if(addr == -1)
+					{
+						cout<<"\nBooking not found!!"<<endl;
+						cout<<"Press any key to continue"<<endl;
+						getch();
+					}
+					else
+					{
+						cout<<endl<<"Deleting the following record:"<<endl;
+						s.dispRec(addr);
+						s.delRec(addr);
+						cout<<"\nBooking Deleted!"<<endl;
+						cout<<"Press any key to continue"<<endl;
+						getch();						
+					}
 					break;
 				
 			case 6: cout<<"\nAre you sure you want to Exit? (Y/N)\n";
