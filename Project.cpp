25e3dc796f18337fd3f12382248df13a447f8756
akshatsaxena1010 <akshatonlyone@gmail.com>
2019-05-09 08:32:36 +0530

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -7,7 +7,8 @@
 #include<iostream>
 
 #define safarifile "saf11.txt"
-#define recsize 40
+#define delfile "safdel.txt"
+#define recsize 50
 #define max 11
 
 using namespace std;
@@ -19,10 +20,10 @@ char keys[50][11];
 class safari
 {
 	public:
-		char bookId[6],name[20];
+		char bookId[6],name[20],contact[11],type[5],num[3];
 		void initial();
-		void reset();
 		void read();
+		int calcCost(int x);
 		void pack();
 		int getHash(char key[]);
 		void display();
@@ -31,6 +32,7 @@ class safari
 		void dispRec(int addr);
 		void delRec(int addr);
 		void modify(int addr);
+		void reset();
 };
 
 void safari::initial()
@@ -45,54 +47,26 @@ void safari::initial()
 		getch();
 		exit(1);
 	}
-	
+	for(i=0; i<max; i++)
+		strcpy(keys[i],"NULL");
 	for(i=0; i<max; i++)
 	{	
 		file.seekg(i*(recsize),ios::beg);
 		int f=file.tellg();
 		file.getline(check, recsize, '|');
-		if(strcmp(check,"#####################################") == 0)
-			strcpy(keys[i], "NULL");
-		else 
+		if(strcmp(check,"###############################################") != 0)
 			strcpy(keys[i],check);		
 		file.getline(dummy, recsize, '\n');
 	}
 	file.close();
 }
 
-void safari::reset()
-{
-	int i,j;
-	for(i=0; i<max; i++)
-		strcpy(keys[i],"NULL");
-	
-	file.open(safarifile);
-	if(!file)
-	{
-		cout<<"\nUnable to open the file";
-		getch();
-		exit(1);
-	}
-		
-	for(i=0;i<max;i++)
-	{
-		file.seekp(i*(recsize),ios::beg);
-		for(j=0;j<recsize-3;j++)
-			file<<"#";
-		file<<"|";
-		file<<"\n";
-	}
-	cout<<"Empty file created!";
-	cout<<"Press any key to continue";
-	file.close();
-	getch();
-}
-
 void safari::read()
 {	
 	char choice;
+	int flag = 0,x;
 	cin.ignore();
-	cout<<"Enter your booking id"<<endl;
+	cout<<"Enter a booking id"<<endl;
 	gets(bookId);
 	while(search(bookId) >= 0)
 	{
@@ -100,12 +74,51 @@ void safari::read()
 		cout<<"Enter another booking id: ";
 		gets(bookId);
 	}
-	cout<<"Enter your name"<<endl;
+	cout<<"Enter the name"<<endl;
 	gets(name);
+	cout<<"Enter the Contact Number"<<endl;
+	gets(contact);
+	cout<<"Enter the type of Safari(Jeep, Boat or Bus)"<<endl;
+	while(flag == 0)
+	{
+		gets(type);
+		if((strcmpi(type,"jeep") == 0) || (strcmpi(type,"boat") == 0) || (strcmpi(type,"bus") == 0))
+			flag = 1;
+		else
+			cout<<"Enter one of the choices: Jeep, Boat or Bus"<<endl;
+	}
+	cout<<"Enter the Number of Passengers: "<<endl;
+	flag = 0;
+	while(flag == 0)
+	{
+		gets(num);
+		sscanf(num, "%d", &x); 
+		if(strcmpi(type,"jeep") == 0)
+			if(x<1 || x>6)
+				cout<<"The number of Passengers in a Jeep must be between 1 and 6. Try again.."<<endl;
+			else
+				flag = 1;
+		if(strcmpi(type,"bus") == 0)
+			if(x<7 || x>20)
+				cout<<"The number of Passengers in a Bus must be between 7 and 20. Try again.."<<endl;
+			else
+				flag = 1;
+		if(strcmpi(type,"boat") == 0)
+			if(x<1 || x>10)
+				cout<<"The number of Passengers in a Boat must be between 1 and 10. Try again.."<<endl;
+			else
+				flag = 1;
+	}
+	
 	system("cls");
 	cout<<"The Details of the Booking are:"<<endl;
 	cout<<"Booking ID: "<<bookId<<endl;
 	cout<<"Name: "<<name<<endl;
+	cout<<"Contact Number: "<<contact<<endl;
+	cout<<"Ride Type: "<<type<<endl;
+	cout<<"Number of Passengers: "<<x<<endl;
+	int cost = calcCost(x);
+	cout<<"Total Cost: "<<cost<<endl;
 	cout<<endl<<"Do you want to confirm the booking? (Y/N)"<<endl;
 	cin>>choice;
 	if(choice == 'Y'|| choice == 'y')
@@ -122,6 +135,16 @@ void safari::read()
 		getch();
 		return;
 	}
+}
+
+int safari::calcCost(int x)
+{
+	if(strcmpi(type,"jeep") == 0)
+		return 500*x;
+	if(strcmpi(type,"bus") == 0)
+		return 300*x;
+	if(strcmpi(type,"boat") == 0)
+		return 1000*x;
 }
 
 void safari::pack()
@@ -145,6 +168,12 @@ void safari::pack()
 	strcpy(buffer, bookId);
 	strcat(buffer,"|");
 	strcat(buffer, name);
+	strcat(buffer,"|");
+	strcat(buffer, contact);
+	strcat(buffer,"|");
+	strcat(buffer, type);
+	strcat(buffer,"|");
+	strcat(buffer, num);
 	strcat(buffer,"|");
 	file.open(safarifile);
 	if(!file)
@@ -172,9 +201,9 @@ int safari::getHash(char key[])
 
 void safari::display()
 {
-	int i;
+	int i, x, cost;
 	char dummy[recsize],check[recsize];
-	cout<<setw(6)<<"bookId"<<setw(20)<<"name"<<endl;
+	cout<<setw(10)<<"Booking ID"<<setw(20)<<"Name"<<setw(20)<<"Contact No."<<setw(10)<<"Type"<<setw(25)<<"Number of Passengers"<<setw(10)<<"Cost"<<endl;
 	file.open(safarifile, ios::in);
 	if(!file)
 	{
@@ -187,11 +216,16 @@ void safari::display()
 		file.seekg(i*(recsize),ios::beg);
 		int f=file.tellg();
 		file.getline(check, recsize, '|');
-		if(strcmp(check,"#####################################") != 0)
+		if(strcmp(check,"###############################################") != 0)
 		{
 			strcpy(bookId,check);
 			file.getline(name, 20, '|');
-			cout<<setw(6)<<bookId<<setw(20)<<name<<endl;
+			file.getline(contact, 11, '|');
+			file.getline(type, 5, '|');
+			file.getline(num, 3, '|');
+			sscanf(num, "%d", &x); 
+			cost = calcCost(x);
+			cout<<setw(10)<<bookId<<setw(20)<<name<<setw(20)<<contact<<setw(10)<<type<<setw(25)<<num<<setw(10)<<cost<<endl;
 		}
 	}
 	cout<<"Press any key to continue"<<endl;
@@ -222,7 +256,7 @@ int safari::search(char skey[6])
 
 void safari::dispRec(int addr)
 {
-
+	int x;
 	file.open(safarifile);
 	if(!file)
 	{
@@ -234,8 +268,17 @@ void safari::dispRec(int addr)
 	int f=file.tellg();
 	file.getline(bookId, 6, '|');
 	file.getline(name, 20, '|');
+	file.getline(contact, 11, '|');
+	file.getline(type, 5, '|');
+	file.getline(num, 3, '|');
 	cout<<"Booking ID:"<<bookId<<endl;
 	cout<<"Name: "<<name<<endl;
+	cout<<"Contact Number: "<<contact<<endl;
+	cout<<"Ride Type: "<<type<<endl;
+	sscanf(num, "%d", &x); 
+	cout<<"Number of Passengers: "<<x<<endl;
+	int cost = calcCost(x);
+	cout<<"Total Cost: "<<cost<<endl;
 	file.close();
 	cout<<"Press any key to continue....";
 	getch();
@@ -259,6 +302,34 @@ void safari::delRec(int addr)
 	strcpy(keys[addr], "NULL");
 }
 
+void safari::reset()
+{
+	int i,j;
+	for(i=0; i<max; i++)
+		strcpy(keys[i],"NULL");
+	
+	file.open(safarifile);
+	if(!file)
+	{
+		cout<<"\nUnable to open the file";
+		getch();
+		exit(1);
+	}
+		
+	for(i=0;i<max;i++)
+	{
+		file.seekp(i*(recsize),ios::beg);
+		for(j=0;j<recsize-3;j++)
+			file<<"#";
+		file<<"|";
+		file<<"\n";
+	}
+	cout<<"Empty file created!";
+	cout<<"Press any key to continue";
+	file.close();
+	getch();
+}
+
 int main()
 {
 	safari s;
@@ -268,7 +339,7 @@ int main()
 	for(;;)
 	{
 		system("cls");
-		cout<<"1. Read\n2. Display\n3. Search\n4. Modify\n5. Delete\n6. Exit\n7. Reset\n";
+		cout<<"1. Enter a new Booking.\n2. Display all the Bookings.\n3. Search for a Booking. (Using Hash Function)\n4. Modify an existing Booking.\n5. Delete a Booking.\n6. Exit the Program.\n7. Reset. \n";
 		cout<<"Enter your Choice: ";
 		cin>>ch;
 		
@@ -336,10 +407,7 @@ int main()
 					}
 					break;
 				
-			case 6: cout<<"\nAre you sure you want to Exit? (Y/N)\n";
-					cin>>ex;
-					if(ex == 'Y' || ex == 'y')
-						exit(0);
+			case 6:	exit(0);
 					break;
 			
 			case 7:	s.reset();
